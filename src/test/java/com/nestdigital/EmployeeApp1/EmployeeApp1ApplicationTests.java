1602649837d51ea0a8b21ea837f0152820bf70d2
Varshini <varshuk01@gmail.com>
2022-10-15 17:34:34 +0530

    message

---
+++ src/test/java/com/nestdigital/EmployeeApp1/EmployeeApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.EmployeeApp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
